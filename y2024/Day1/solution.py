fe2cae4c19dc3e8dc3cb9ef6abe8bd4269f59c56
Emmanuel Annan <nii.annan@scribeaccounts.com>
2024-12-01 23:13:16 +0000

    improved solution for dy 1

--- y2024/Day1/solution.py
+++ y2024/Day1/solution.py
@@ -1,25 +1,24 @@
 # Advent of Code 2024 - Day 1
 def solve_part1(input_data):
-    input_list = input_data.split('\n')
     left =[]
     right=[]
-    for pair in input_list:
-        (a,b) = pair.split()
-        left.append(int(a))
-        right.append(int(b))
+
+    for pair in input_data.strip().split('\n'):
+        (a,b) = map(int,pair.split())
+
+        left.append(a)
+        right.append(b)
+
     left.sort()
     right.sort()
 
-    diff=[abs(left-right) for left,right in zip(left,right)]
-    return sum(diff)
+    return sum(abs(left-right) for left,right in zip(left,right))
 
 def solve_part2(input_data):
-    input_list = input_data.split('\n')
     left =[]
     right=[]
-    for pair in input_list:
-        (a,b) = pair.split()
-        left.append(int(a))
-        right.append(int(b))
-    multiple = [right.count(num)*num for num in left]
-    return sum(multiple)
+    for pair in input_data.split('\n'):
+        (a,b) = map(int, pair.split())
+        left.append(a)
+        right.append(b)
+    return sum(right.count(num)*num for num in left)
